b2371b9b966501b5cae4fadbb9aad4669ad43e48
Huang Yi-Ming <ymhuang@citi.sinica.edu.tw>
2014-12-08 15:59:03 +0800

    Fix variables

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -4,22 +4,26 @@ class EventTest extends PHPUnit_Framework_TestCase
 {
     public function testReserve()
     {
-        $id = 1; 
-        $name = '活動1';
-        $start_date = '2014-12-24 18:00:00';
-        $end_date = '2014-12-24 20:00:00';
-        $deadline = '2014-12-23 23:59:59';
-        $attendee_limit = 10;
+        $eventId = 1; 
+        $eventName = '活動1';
+        $eventStartDate = '2014-12-24 18:00:00';
+        $eventEndDate = '2014-12-24 20:00:00';
+        $eventDeadline = '2014-12-23 23:59:59';
+        $eventAttendeeLimit = 10;
+        $event = new \PHPUnitEventDemo\Event($eventId, $eventName, $eventStartDate, 
+            $eventEndDate, $eventDeadline, $eventAttendeeLimit);
 
-        $event = new \PHPUnitEventDemo\Event($id, $name, $start_date, 
-            $end_date, $deadline, $attendee_limit);
-        $user = new \PHPUnitEventDemo\User(1, 'User1', 'user1@openfoundry.org');
+        $userId = 1;
+        $userName = 'User1';
+        $userEmail = 'user1@openfoundry.org';
+        $user = new \PHPUnitEventDemo\User($userId, $userName, $userEmail);
 
         // 使用者報名活動
         $event->reserve($user);
         
+        $expectedNumber = 1;
         // 預期報名人數
-        $this->assertEquals(1, $event->getAttendeeNumber());
+        $this->assertEquals($expectedNumber, $event->getAttendeeNumber());
         // 報名清單中有已經報名的人
         $this->assertContains($user, $event->attendees);
     }
