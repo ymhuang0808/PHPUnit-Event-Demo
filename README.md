f11cfc78173840ee707bfa280bbe2984afdaf4f7
Huang Yi-Ming <ymhuang@citi.sinica.edu.tw>
2014-12-08 15:52:02 +0800

    Add some decriptions in README.md

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ PHPUnit-Event-Demo
 * src - 程式
 * tests - 測試程式
 * vendor - Composer 套件
+
+### 說明
+
+* src/PHPUnitEventDemo/
+    * Event.php - 活動類別
+    * User.php - 使用者類別
+* tests/
+    * EventTest.php - 測試 `Event` 類別 
---
+++ src/PHPUnitEventDemo/Event.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PHPUnitEventDemo;
+
+class Event
+{
+    public $id;
+    public $name;
+    public $start_date;
+    public $end_date;
+    public $deadline;
+    public $attendee_limit;
+    public $attendees = array();
+
+    public function __construct($id, $name, $start_date, $end_date, $deadline, $attendee_limit)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->start_date = $start_date;
+        $this->end_date = $end_date;
+        $this->deadline = $deadline;
+        $this->attendee_limit = $attendee_limit;
+    }
+
+    public function reserve($user)
+    {
+        // 使用者報名
+        $this->attendees[] = $user;
+    }
+
+    public function getAttendeeNumber()
+    {
+        // 取得報名人數
+        return sizeof($this->attendees);
+    }
+}
